c1b3cf021e8876061a201491d2c08749a9e1ee48
Khắc Nhật Bùi <quochuy2717@gmail.com>
2020-05-06 23:19:30 +0700

    test git commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ export default class App extends Component {
        
         <Avatar imageUrl='https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=3759e09a5b9fbe53088b23c615b6312e'/>
         <Avatar imageUrl='https://tinyfac.es/data/avatars/AEF44435-B547-4B84-A2AE-887DFAEE6DDF-200w.jpeg' />
+
+        <p>test git</p>
   
 
 
